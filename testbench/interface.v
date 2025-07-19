1c54f022c3efaa62f86c4e3a2bd92e9dab46a7a9
Anagha Sujith <aka.anaghasujith@gmail.com>
2025-07-19 16:45:48 +0530

    updated testbench/interface.sv

--- testbench/interface.v
+++
@@ -1,9 +0,0 @@
-interface FFT4If;
-    
-    logic [15:0] x0_re, x0_im, x1_re, x1_im, x2_re, x2_im, x3_re, x3_im;
-    
-    logic [15:0] y0_re, y0_im, y1_re, y1_im, y2_re, y2_im, y3_re, y3_im;
-    
-    logic clk; // Clock signal
-
-endinterface 
